006c33f48a4a386ffe1d9566778e8e23f6a432bd
C0dE_brEakEr <90004033+Irfanul1925021@users.noreply.github.com>
2022-09-25 21:40:08 +0600

    Update BookAnalysis.java

--- source/AllInfoDisplay/BookAnalysis.java
+++ source/AllInfoDisplay/BookAnalysis.java
@@ -1,1 +1,22 @@
+package InformationDisplay;
 
+import FilePackage.DateTimeWriter;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.scene;
+
+import java.io.IOException;
+
+public class BookAnalysis extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        String clsName = this.getClass().getSimpName();
+ 
+        Parent parent = FXMLLoader.load(getClass().getResource("file.fxml"));
+        primaryStage.setScene(new Scene(parent, 1500, 600));
+        primaryStage.show();
+    }
+}
