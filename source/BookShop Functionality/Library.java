72bff8dac8ebcc46941e9ec04d1d594894456432
C0dE_brEakEr <90004033+Irfanul1925021@users.noreply.github.com>
2022-09-25 21:46:02 +0600

    Add files via upload

---
+++ source/BookShop Functionality/Library.java
@@ -0,0 +1,164 @@
+package LibraryFunctionality;
+
+/**Created:
+ Alhamdulillah
+ Happy Coding!
+ To be continued.......**/
+
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Library {
+    private SimpleStringProperty userID;
+    private SimpleStringProperty userName;
+    private SimpleStringProperty userInstitute;
+    private SimpleStringProperty userEducationLevel;
+    private SimpleStringProperty userClass;
+
+    public Library(SimpleStringProperty userID,
+            SimpleStringProperty userName,
+            SimpleStringProperty userInstitute,
+            SimpleStringProperty userEducationLevel,
+            SimpleStringProperty userClass) {
+        this.userID = userID;
+        this.userName = userName;
+        this.userInstitute = userInstitute;
+        this.userEducationLevel = userEducationLevel;
+        this.userClass = userClass;
+    }
+
+    /*
+     * public Library(String userID1, String userName1,
+     * String userInstitute1, String userEducationLevel1,
+     * String userClass1) {
+     * this.userID1 = userID1;
+     * this.userName1 = userName1;
+     * this.userInstitute1 = userInstitute1;
+     * this.userEducationLevel1 = userEducationLevel1;
+     * this.userClass1 = userClass1;
+     * }
+     */
+
+    /*
+     * private String userID1;
+     * private String userName1;
+     * private String userInstitute1;
+     * private String userEducationLevel1;
+     * private String userClass1;
+     * 
+     * public String getUserID1() {
+     * return userID1;
+     * }
+     * 
+     * public void setUserID1(String userID1) {
+     * this.userID1 = userID1;
+     * }
+     * 
+     * public String getUserName1() {
+     * return userName1;
+     * }
+     * 
+     * public void setUserName1(String userName1) {
+     * this.userName1 = userName1;
+     * }
+     * 
+     * public String getUserInstitute1() {
+     * return userInstitute1;
+     * }
+     * 
+     * public void setUserInstitute1(String userInstitute1) {
+     * this.userInstitute1 = userInstitute1;
+     * }
+     * 
+     * public String getUserEducationLevel1() {
+     * return userEducationLevel1;
+     * }
+     * 
+     * public void setUserEducationLevel1(String userEducationLevel1) {
+     * this.userEducationLevel1 = userEducationLevel1;
+     * }
+     * 
+     * public String getUserClass1() {
+     * return userClass1;
+     * }
+     * 
+     * public void setUserClass1(String userClass1) {
+     * this.userClass1 = userClass1;
+     * }
+     */
+
+    public String getUserID() {
+        return userID.get();
+    }
+
+    public SimpleStringProperty userIDProperty() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID.set(userID);
+    }
+
+    public String getUserName() {
+        return userName.get();
+    }
+
+    public SimpleStringProperty userNameProperty() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName.set(userName);
+    }
+
+    public String getUserInstitute() {
+        return userInstitute.get();
+    }
+
+    public SimpleStringProperty userInstituteProperty() {
+        return userInstitute;
+    }
+
+    public void setUserInstitute(String userInstitute) {
+        this.userInstitute.set(userInstitute);
+    }
+
+    public Library(String s1, String s2, String s3, String s4, String s5) {
+
+        userID = new SimpleStringProperty(s1);
+        userName = new SimpleStringProperty(s2);
+        userInstitute = new SimpleStringProperty(s3);
+        userEducationLevel = new SimpleStringProperty(s4);
+        userClass = new SimpleStringProperty(s5);
+
+    }
+
+    public String getUserEducationLevel() {
+        return userEducationLevel.get();
+    }
+
+    public SimpleStringProperty userEducationLevelProperty() {
+        return userEducationLevel;
+    }
+
+    public void setUserEducationLevel(String userEducationLevel) {
+        this.userEducationLevel.set(userEducationLevel);
+    }
+
+    public String getUserClass() {
+        return userClass.get();
+    }
+
+    public SimpleStringProperty userClassProperty() {
+        return userClass;
+    }
+
+    public void setUserClass(String userClass) {
+        this.userClass.set(userClass);
+    }
+
+}
+/**Created:
+ Alhamdulillah
+ Happy Coding!
+ To be continued.......**/
